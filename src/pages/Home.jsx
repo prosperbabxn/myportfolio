c91a75d677ae66a9b1980cd61cd4c7dd00c4d321
prosper babalola <prosperbabalola@gmail.com>
2024-10-30 14:42:52 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Herosection from '../components/Herosection'
 import Middleman from '../components/middleman'
 
+
 export default function Home() {
   return (
     <div>
       <Herosection/>
-      <Middleman/>
+      <Middleman />
     </div>
   )
 }
